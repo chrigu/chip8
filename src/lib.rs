ef2ab32123a923da8fad3fef1a66c2163840216a
Christian Cueni <christian.cueni@iterativ.ch>
2020-02-25 19:30:02 +0100

    Add wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 pub mod cpu;
 pub mod display;
 
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, chip8!");
+}
+
 pub fn decompile(binary_buffer: &Vec<u8>) {
     println!("{:?}", binary_buffer);
 
