4a9da7950fdf9e01998fb9c257bd907f132700a0
Christian Cueni <christian.cueni@iterativ.ch>
2020-03-17 20:40:35 +0100

    Add basic display

--- www/index.js
+++ www/index.js
@@ -1,10 +1,18 @@
 import { memory } from "chip8/chip8_bg";
 import {Cpu, init_panic_hook} from "chip8";
 
+const displayWidth = 64;
+const displayHeight = 32;
+const pixelSize = 4;
+
 init_panic_hook();
 const cpu = Cpu.new();
 const displayPtr = cpu.read_display();
-const display = new Uint8Array(memory.buffer, displayPtr, 32 * 64);
+const display = new Uint8Array(memory.buffer, displayPtr, displayHeight * displayWidth);
+
+var displayElement = document.getElementsByClassName('display')[0];
+console.log(displayElement);
+const context = displayElement.getContext("2d");
 
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
@@ -40,25 +48,36 @@ function handleFileSelect(evt) {
 
     reader.readAsArrayBuffer(files[0])
 
-    
-  }
+}
 
-  function tick () {
-      cpu.emulate_cycle();
-      console.log(cpu.read_pc());
-      const disp2 = new Uint8Array(memory.buffer, displayPtr, 32 * 64);
-      let text = '';
-      for(let i = 0;i < 32 * 64;i++) {
-        text += disp2[i];
-          if (i > 0 && i % 64 === 0) {
-              text += '\n';
-          }
-      }
+function tick () {
+    cpu.emulate_cycle();
+    console.log(cpu.read_pc());
+    const disp2 = new Uint8Array(memory.buffer, displayPtr, displayWidth * displayHeight);
+    let text = '';
+    for(let i = 0;i < 32 * 64;i++) {
+    text += disp2[i];
+        if (i > 0 && i % 64 === 0) {
+            text += '\n';
+        }
+    }
+    render(disp2);
 
-      document.getElementById('display').innerHTML = text;
-  }
+    document.getElementById('display').innerHTML = text;
+}
 
-  document.getElementById('rom').addEventListener('change', handleFileSelect, false);
-  
-  
-  document.getElementById('tick').addEventListener('click', tick, false);
+function render (pixels) {
+    for(let i = 0;i < displayHeight * displayWidth;i++) {
+        let x = pixelSize * (i % displayWidth);
+        let y = pixelSize * Math.floor(i / displayWidth);
+        drawPixel(x, y, pixels[i]);
+    }
+}
+
+function drawPixel(x, y, set) {
+    context.fillStyle = set ? 'rgb(76, 179, 91)' : 'rgb(0, 0, 0)';
+    context.fillRect(x, y, pixelSize, pixelSize); 
+}
+
+document.getElementById('rom').addEventListener('change', handleFileSelect, false);
+document.getElementById('tick').addEventListener('click', tick, false);
