87465538ee8f48a842da81429e3ddbe1c1074bd5
Christian Cueni <christian.cueni@iterativ.ch>
2020-04-08 20:20:14 +0200

    Add selection

--- web/src/components/RomSelection.vue
+++ web/src/components/RomSelection.vue
@@ -2,29 +2,79 @@
   <div class="rom-selection">
     <div class="rom-list">
       <div class="select">
-  <select>
-    <option>Airplane</option>
-    <option>Breakout</option>
-    <option>Chip8</option>
-    <option>Guess</option>
-    <option>IBM Logo</option>
-    <option>Landing</option>
-    <option>Maze</option>
-  </select>
-</div>
+        <select v-model="selectedRom" @change="romSelected">
+          <option v-for="rom in roms" :key="rom.name" :value="rom.file">{{rom.name}}</option>
+        </select>
+      </div>
     </div>
     <RomUploader />
   </div>
 </template>
 
 <script>
-
+import { mapActions } from 'vuex'
 import RomUploader from '@/components/RomUploader.vue'
 
 export default {
   name: 'RomSelection',
   components: {
     RomUploader
+  },
+  data () {
+    return {
+      roms: [
+        {
+          'name': 'Airplane',
+          'file': 'Airplane.ch8'
+        },
+        {
+          'name': 'Breakout',
+          'file': 'Breakout.ch8'
+        },
+        {
+          'name': 'Chip 8 Picture',
+          'file': 'Chip8_Picture.ch8'
+        },
+        {
+          'name': 'Guess',
+          'file': 'Guess.ch8'
+        },
+        {
+          'name': 'IBM Logo',
+          'file': 'ibm_logo.ch8'
+        },
+        {
+          'name': 'Landing',
+          'file': 'Landing.ch8'
+        },
+        {
+          'name': 'Maze',
+          'file': 'maze.ch8'
+        },
+        {
+          'name': 'Pong',
+          'file': 'Pong.ch8'
+        },
+        {
+          'name': 'Trip8 Demo',
+          'file': 'Trip8_Demo.ch8'
+        },
+        {
+          'name': 'Worm',
+          'file': 'Worm.ch8'
+        },
+      ],
+      selectedRom: 'Chip8_Picture.ch8'
+    }
+  },
+  methods: {
+    ...mapActions(['setRom']),
+    romSelected(other) {
+      const url = `/roms/${this.selectedRom}`;
+      fetch(url)
+      .then(data => data.blob())
+      .then(data => this.setRom(data))
+    }
   }
 }
 </script>
